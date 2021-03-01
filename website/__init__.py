31cb3a073e32cdac91b8ffe88eba57d663f83f9a
cerisilv123 <60038915+cerisilv123@users.noreply.github.com>
2021-03-01 17:10:58 +0000

    Added code to flash error messages on signup and wrote code to create a database in init.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,39 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 from.views import view
 from.auth import auth
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     """ This function creates the flask app when 
     this module is loaded. It also configures the 
-    'secret key' which is used to sign session cookies 
-    and registers the blueprints used for the routes.  
+    'secret key' which is used to sign session cookies, 
+    registers the blueprints used for the routes and 
+    initialises the database.  
     """
 
     app=Flask(__name__)
-    app.config['SECRET_KEY'] = 'gfyrifhnrgxyrfngyufggrege'
+    app.config["SECRET_KEY"] = "gfyrifhnrgxyrfngyufggrege" # Make this more secure!
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
+
     app.register_blueprint(view, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/auth')
 
+    from .models import User
+    create_database(app)
+
     return app
+
+def create_database(app):
+    """ This function checks to see if a path to 
+    the database already exists using the os module. 
+    If a path does not exist, the database is created. 
+    """
+
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database")
--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=["POST", "GET"])
 def login():
     return render_template('login.html')
 
-@auth.route('/signup')
+@auth.route('/signup', methods=["POST", "GET"])
 def signup():
-    return render_template('signup.html')
+    if request.method == "POST":
+        signup_email = request.form["emailInput"]
+        signup_username = request.form["usernameInput"]
+        signup_password1 = request.form["passwordInput1"]
+        signup_password2 = request.form["passwordInput2"]
+
+        if len(signup_email) < 4:
+            flash("Email length must be greater than 4 characters", category="error") 
+        elif len(signup_username) < 4:
+            flash("Username length must be greater than 4 characters", category="error")
+        elif len(signup_password1) < 7:
+            flash("Password length must be greater than 7 characters", category="error")
+        elif signup_password1 != signup_password2:
+            flash("Passwords do not match", category="error")
+        else:
+            print("#Write Code functionality to add details to the database here")
+
+        return redirect(url_for("auth.signup"))
+    else: 
+        return render_template("signup.html")
