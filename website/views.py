c6567db257ffd204c52cea2fd7a9235dd881e19a
cerisilv123 <60038915+cerisilv123@users.noreply.github.com>
2021-07-24 12:38:29 +0100

    Wrote code to allow user to post to a community page in the community_page view

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, flash, redirect, url_for, request
 from flask_login import login_required, current_user
-from .models import User, Community
+from .models import User, Community, Usercommunity, Post
 from . import db
 
 view = Blueprint('view', __name__)
@@ -118,15 +118,47 @@ def create_community():
     else: 
         return render_template("create_community.html")
 
-@view.route('/community_page/<community_name>')
+@view.route('/community_page/<community_name>', methods=["POST", "GET"])
 @login_required
 def community_page(community_name):
+    
+    is_member = False
+    community_details = Community.query.filter_by(name=community_name).first()
+    user_community_details = Usercommunity.query.filter_by(user_id=current_user.id, community_id=community_details.id).first()
+    if user_community_details is not None:
+            is_member = True
 
     if request.method == 'POST':
-        return render_template("community_page.html")
-    else: 
-        community_details = Community.query.filter_by(name=community_name).first()
+        if request.form["hiddenCommunityPageForm"] == "postMessage":
+            message = request.form["communityPagePostMessage"]
+            heading = request.form["communityPagePostMessageHeading"]
+            new_community_post = Post(user_id=current_user.id, community_id=community_details.id, text=message, heading=heading)
+            db.session.add(new_community_post)
+            db.session.commit()
+        else:
+            if is_member: 
+                db.session.delete(user_community_details)
+                db.session.commit()
+            else: 
+                new_user_community = Usercommunity(user_id=current_user.id, community_id=community_details.id)
+                db.session.add(new_user_community)
+                db.session.commit()
+        return redirect(url_for("view.community_page", community_name=community_name))
+    else:  
         community_about = community_details.about
         community_category = community_details.category
 
-        return render_template("community_page.html", community_name=community_name, community_about=community_about, community_category=community_category)
+        community_post_details = Post.query.filter_by(community_id=community_details.id)
+        community_posts = []
+        for post in community_post_details:
+            community_post = {
+                "heading":post.heading, 
+                "text":post.text, 
+                "date":post.date, 
+                "user_id":post.user_id
+            }
+            community_posts.append(community_post)
+
+        print(community_posts)
+
+        return render_template("community_page.html", community_name=community_name, community_about=community_about, community_category=community_category, is_member=is_member, community_posts=community_posts)
